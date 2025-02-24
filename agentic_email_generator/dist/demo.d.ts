0a6ff287512f52fa8271cf41a8000719435a4990
travis martin <travismartin266@gmail.com>
2025-02-24 08:19:50 -0400

    clean up

--- agentic_email_generator/dist/demo.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Demo file showcasing the Agentic Email Generator
- * This demo targets generating an email to Elon Musk for business development
- */
-export {};
--- agentic_email_generator/dist/models/models.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Core data models for the Agentic Email Generator system.
- * This file contains TypeScript interfaces that define the structure of all
- * major entities in the system.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
